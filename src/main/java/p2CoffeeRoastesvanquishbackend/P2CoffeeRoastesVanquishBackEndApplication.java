443de79806b666673318d6afe0ed2269123ce237
CodeCademmy <rqlmoura@yahoo.com>
2022-01-13 14:55:39 -0800

    Update

---
+++ src/main/java/p2CoffeeRoastesvanquishbackend/P2CoffeeRoastesVanquishBackEndApplication.java
@@ -0,0 +1,13 @@
+package p2CoffeeRoastesvanquishbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2CoffeeRoastesVanquishBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2CoffeeRoastesVanquishBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/p2CoffeeRoastesvanquishbackend/P2CoffeeRoastesVanquishBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package p2CoffeeRoastesvanquishbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2CoffeeRoastesVanquishBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
