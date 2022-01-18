62fbb29ddd6cdaf1385f38ef274520dc79afa717
henryd0615 <henryd0615@gmail.com>
2022-01-18 19:54:33 -0400

    Added some Exceptions and Edited some code

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -4,12 +4,14 @@ import java.util.Set;
 
 import org.apache.catalina.User;
 
+import Exceptions.IncorrectCredentialsException;
+import Exceptions.UsernameAlreadyExistsException;
 import p2CoffeeRoastesvanquishbackend.beans.Plan;
 
 public interface UserService 
 {
-	public User register(User newUser);
-	public User logIn(String username, String password);
+	public User register(User newUser) throws UsernameAlreadyExistsException;
+	public User logIn(String username, String password) throws IncorrectCredentialsException;
 	public User getUserById(int id);
 	public User updateUser(User userToUpdate);
 	public User choosePlan(int planId, User user);
