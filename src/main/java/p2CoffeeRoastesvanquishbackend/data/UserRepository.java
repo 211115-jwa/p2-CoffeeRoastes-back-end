ec7f6fed6a144fe5bce03a9dfcc9277241d1bcb8
Gamaliel Burgos <gamyburgos@gmail.com>
2022-01-19 23:26:13 -0800

    Reverting to 962c8b for connectivity reasons

---
+++ src/main/java/p2CoffeeRoastesvanquishbackend/data/UserRepository.java
@@ -0,0 +1,13 @@
+package p2CoffeeRoastesvanquishbackend.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import p2CoffeeRoastesvanquishbackend.beans.User;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	public User findByUsername(String username);
+	public User findById(int id);
+}
