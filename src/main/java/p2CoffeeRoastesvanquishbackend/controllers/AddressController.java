7bd646353a5864ce6720deb5d522e17f24bdf7c4
Gamaliel Burgos <gamyburgos@gmail.com>
2022-01-26 19:16:55 -0800

    changes for 1/26

--- src/main/java/p2CoffeeRoastesvanquishbackend/controllers/AddressController.java
+++ src/main/java/p2CoffeeRoastesvanquishbackend/controllers/AddressController.java
@@ -1,6 +1,10 @@
 package p2CoffeeRoastesvanquishbackend.controllers;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,55 +14,62 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import p2CoffeeRoastesvanquishbackend.beans.Address;
 import p2CoffeeRoastesvanquishbackend.exceptions.IncorrectAddressExeption;
 import p2CoffeeRoastesvanquishbackend.services.AddressService;
-//import p2CoffeeRoastesvanquishbackend.services.UserService;
 
 
 
+@Configuration
 @RestController
 @RequestMapping(path="/address")
 @CrossOrigin(origins="http://localhost:4200")
-
-
 public class AddressController {
 	
 	
-	@Autowired
+
 	private AddressService addressService;
 	
-
 	
-	  @Autowired
+	  public AddressController() {
 	  
-	  public AddressController(AddressService addressService) {
-	  this.addressService= addressService;
+	  super();
 	  
 	  }
-	  
-	  
 	 
 	
 	
-//	@PostMapping (path = "/address/{id}")
-//	public ResponseEntity<Void> addAddress(@RequestBody Address newAddress){
-//		
-//		if (newAddress !=null) {
-//			addressService.addNewAddress(newAddress);
-//			return ResponseEntity.status(HttpStatus.CREATED).build();
-//	}
-//	return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-//	}
+		@Autowired
+	  public AddressController(AddressService addressService) {
+	  this.addressService=addressService; }
+	 
+	
+	
+	@PostMapping (path = "/add")
+	public ResponseEntity<Void> addAddress(@RequestBody Address newAddress){
+		
+		if (newAddress !=null) {
+	
+			addressService.addNewAddress(newAddress);
+			return ResponseEntity.status(HttpStatus.CREATED).build();
+	}else {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+		
+
+	}
 	
 	
 	
-		@GetMapping(path="/address/{id}")
-		public ResponseEntity<Address> LookUpAddress(@RequestBody String token,
+		@GetMapping(path="/{id}")
+		public ResponseEntity<Set<Address>> LookUpAddress(@RequestBody String token,
 				@PathVariable int user_id) {
-			Address UserAddressId = addressService.getLookUpAddressByUser(user_id);
+			Set<Address> UserAddressId = addressService.getLookUpAddressByUser(user_id);
 			if (UserAddressId!=null) {
 				return ResponseEntity.ok(UserAddressId);
 			} else {
@@ -69,12 +80,17 @@ public class AddressController {
 		}
 	
 
-	 @DeleteMapping(path = "/address/{id}")
+	 @DeleteMapping(path = "/{id}")
 	public ResponseEntity<String> deleteAddress(@RequestBody int id) throws IncorrectAddressExeption {
 		Address addressDeletedId = addressService.deleteAddressById(id);
 		String token = Integer.toString(addressDeletedId.getAddress_id());
 		return ResponseEntity.ok(token);
 
 }
+	 
+	
+		
+	}
 
-}
+
+
